6c8318b75630ae1b5e4a70c6cea7d0b4ec61363a
KeanBP36 <git@keanbp.com>
2026-09-18 17:26:07 +0200

    fixed unstbale sync aliases

--- hosts/common.nix
+++ hosts/common.nix
@@ -196,8 +196,8 @@
       # ─────────────────────────────────────────────
       alias nixpullunstable='cd /etc/nixos-config && git switch unstable && git pull --ff-only'
       alias nixpushunstable='cd /etc/nixos-config && git switch unstable && git status && git add . && read -p "Commit message: " msg && git commit -m "$msg" && git push origin unstable'
-      alias nixcheckunstable='cd /etc/nixos-config && git switch unstable && git fetch origin && git log --oneline unstable..origin/testing'
-      alias nixsyncunstable='cd /etc/nixos-config && git switch unstable && git fetch origin && git cherry-pick $(git rev-list --reverse unstable..origin/testing)'
+      alias nixcheckunstable='cd /etc/nixos-config && git switch unstable && git fetch origin && git log --oneline HEAD..origin/testing'
+      alias nixsyncunstable='cd /etc/nixos-config && git switch unstable && git fetch origin && git merge origin/testing'
 
       # ─────────────────────────────────────────────
       # Git — main branch
